3c7caae99d3d759233c7aecbbd535859342184a3
Aashmit Shrestha <aashmit13@gmail.com>
2023-04-13 09:12:16 +0530

    added graph to the web app

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ data_training_array=scaler.fit_transform(data_training)
 
 #Load Model
 model=load_model('keras_model.h5')
-
+gru_model=load_model('GRU.h5')
 past_100_days=data_training.tail(100)
 final_df=past_100_days.append(data_testing,ignore_index=True)
 
@@ -54,18 +54,39 @@ for i in range(100,input_data.shape[0]):
 x_test,y_test=np.array(x_test),np.array(y_test)
 
 y_predicted=model.predict(x_test)
+y_predicted2=gru_model.predict(x_test)
 scaler=scaler.scale_
 scale_factor=1/scaler[0]
 y_predicted=y_predicted*scale_factor
 y_test=y_test*scale_factor
+y_predicted2=y_predicted2*scale_factor
 
 
 st.subheader("Predictions vs Original")
 fig=plt.figure(figsize=(12,6))
 plt.plot(y_test,'b',label='Original Price')
-plt.plot(y_predicted,'r',label='Predicted Price')
+plt.plot(y_predicted,'r',label='Predicted Price using LSTM')
+plt.plot(y_predicted2,'y',label='Predicted Price using GRU')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
 plt.show()
 st.pyplot(fig)
+
+
+import math
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error,mean_absolute_percentage_error
+def return_error(test,predicted):
+    rmse = math.sqrt(mean_squared_error(test, predicted))
+    mae=mean_absolute_error(test,predicted)
+    mape=mean_absolute_percentage_error(test,predicted)*100
+    return rmse,mae,mape
+
+rmse_LSTM=return_error(y_test,y_predicted)
+rmse_GRU=return_error(y_test,y_predicted2)
+error_df=pd.DataFrame(columns=('Model','RMSE'))
+error_data=[('LSTM',rmse_LSTM[0],rmse_LSTM[1],rmse_LSTM[2]),('GRU',rmse_GRU[0],rmse_GRU[1],rmse_GRU[2])]
+error_df=pd.DataFrame(error_data,columns=('Model','RMSE','MAE','MAPE'))
+st.subheader("Error Evaluation")
+st.write(error_df)
